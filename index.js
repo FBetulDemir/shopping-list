2943e433b38cdda614a39a8871838b64f78c1a00
FBetulDemir <fatmabetuld@gmail.com>
2024-07-05 12:56:17 +0200

    Created list with user input in index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // https://shopping-list-40f3b-default-rtdb.europe-west1.firebasedatabase.app/
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const listDisplay = document.getElementById("list-display")
 
 const appSettings = {
     databaseURL: "https://shopping-list-40f3b-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -13,3 +14,20 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
+
+
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+
+    inputFieldEl.value = ""
+    
+    listDisplay.innerHTML += 
+        `
+            <ul>
+                <li>${inputValue}</li>
+            </ul>
+        `
+})
